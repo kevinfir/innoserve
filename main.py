caf4e977a546cebd1874d21ab73057a60abb355f
kevinfir <95353256+kevinfir@users.noreply.github.com>
2024-10-02 23:34:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from database import get_connection
 import user
 import superuser_login
 import principal
-import time
 # 建立資料庫連接
 conn = get_connection('users.db')
 c = conn.cursor()
@@ -32,15 +31,15 @@ with open('styles.css') as f:
 
 # 建立選項卡來切換身份驗證模式
 auth_mode = st.sidebar.selectbox('選擇身份驗證模式', ['使用者', '管理員'])
-while True:
-    if auth_mode == '使用者':
-        user_auth_mode = st.sidebar.selectbox('選擇身份驗證模式', ['登入', '註冊'])
-        if user_auth_mode == '登入':
-            user.user_login()
-        elif user_auth_mode == '註冊':
-            user.user_register()
 
-        # 檢查是否已經登入
+if auth_mode == '使用者':
+    user_auth_mode = st.sidebar.selectbox('選擇身份驗證模式', ['登入', '註冊'])
+    if user_auth_mode == '登入':
+        user.user_login()
+    elif user_auth_mode == '註冊':
+        user.user_register()
+    while True:
+    # 檢查是否已經登入
         if 'authenticated' in st.session_state and st.session_state['authenticated']:
             st.write("歡迎來到主頁面！")
             # 在這裡可以添加導航到另一個 Streamlit 應用程式的邏輯
@@ -48,15 +47,14 @@ while True:
                 st.write("導航到另一個應用程式...")
                 import order
                 order
-        else:
-            st.write("請先登入。")
+            else:
+                st.write("請先登入。")
 
-    elif auth_mode == '管理員':
-        if 'admin_authenticated' not in st.session_state:
-            st.session_state['admin_authenticated'] = False
+elif auth_mode == '管理員':
+    if 'admin_authenticated' not in st.session_state:
+        st.session_state['admin_authenticated'] = False
 
-        if not st.session_state['admin_authenticated']:
-            superuser_login.admin_login()
-        else:
-            superuser_login.admin_dashboard()
-    time.sleep(5)
+    if not st.session_state['admin_authenticated']:
+        superuser_login.admin_login()
+    else:
+        superuser_login.admin_dashboard()
