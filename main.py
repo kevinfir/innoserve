aa94e7aa47d8a15323534de69b99a8761b98c8dd
kevinfir <95353256+kevinfir@users.noreply.github.com>
2024-10-02 23:22:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,18 +38,18 @@ if auth_mode == '使用者':
         user.user_login()
     elif user_auth_mode == '註冊':
         user.user_register()
-    
+ 
     # 檢查是否已經登入
     if 'authenticated' in st.session_state and st.session_state['authenticated']:
-            st.write("歡迎來到主頁面！")
-    while True:
-            # 在這裡可以添加導航到另一個 Streamlit 應用程式的邏輯
-        if st.button('前往另一個應用程式'):
-            st.write("導航到另一個應用程式...")
-            import order
-            order
-        else:
-            st.write("請先登入。")
+        st.write("歡迎來到主頁面！")
+        while True:
+                # 在這裡可以添加導航到另一個 Streamlit 應用程式的邏輯
+            if st.button('前往另一個應用程式'):
+                st.write("導航到另一個應用程式...")
+                import order
+                order
+            else:
+                st.write("請先登入。")
 
 elif auth_mode == '管理員':
     if 'admin_authenticated' not in st.session_state:
